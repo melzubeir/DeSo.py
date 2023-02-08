a3c6b12c2fd512ad41c257ccad86eabb1133c51d
Mo Elzubeir <elzubeir@gmail.com>
2023-02-08 14:24:42 -0600

    always be having fun

--- deso/__init__.py
+++ deso/__init__.py
@@ -1,10 +1,10 @@
 from deso import Identity
-from . import Media
-from deso.Metadata import Metadata
-from deso.Social import Social
-from deso.Derived import Derived
-from deso.Posts import Posts
-from deso.Trade import Trade
-from deso.User import User
+from deso import Media
+from deso import Metadata
+from deso import Social
+from deso import Derived
+from deso import Posts
+from deso import Trade
+from deso import User
 
 __all__ = tuple(k for k in locals() if not k.startswith("_"))
