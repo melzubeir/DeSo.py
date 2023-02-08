36a490d6e55d8f803009f5a93b3f02e4453b5d29
Mo Elzubeir <elzubeir@gmail.com>
2023-02-08 14:19:28 -0600

    let's have fun

--- deso/__init__.py
+++ deso/__init__.py
@@ -1,5 +1,5 @@
-from Identity import Identity
-from deso.Media import Media
+from deso import Identity
+from . import Media
 from deso.Metadata import Metadata
 from deso.Social import Social
 from deso.Derived import Derived
